834f41c5cc3522fee9ec4dde9f9957ca3138d34f
43962 <qianqinggongben@gmail.com>
2022-10-24 13:13:43 +0000

    アニメーション

--- Jquary-Lesson/app.js
+++ Jquary-Lesson/app.js
@@ -2,8 +2,19 @@
 //   $('body').html('<h1>Hello jQuery!!</h1>');
 // });
 
-.box1 {
-  background-color: #FF0000;
-  height: 200px;
-  width: 200px;
-}
+// .box1 {
+//   background-color: #FF0000;
+//   height: 200px;
+//   width: 200px;
+// }
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+$(function(){
+  $('.box1').slideDown();
+});
